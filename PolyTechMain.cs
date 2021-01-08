04680f87acbb508842671797f535baddb5f9c0bf
Conqu3red <Conqu3red@users.noreply.github.com>
2021-01-08 19:29:04 +0000

    version bump

--- PolyTechMain.cs
+++ PolyTechMain.cs
@@ -23,7 +23,7 @@ namespace PolyTechFramework
         public new const string
             PluginGuid = "polytech.polytechframework",
             PluginName = "PolyTech Framework",
-            PluginVersion = "0.8.3";
+            PluginVersion = "0.9.0";
         private static BindingList<PolyTechMod>
             noncheatMods = new BindingList<PolyTechMod> { },
             cheatMods = new BindingList<PolyTechMod> { };
